da5ab0f75986dd93053f492e4235f46ce29222f4
Jermaine Francoeur <jermerf@gmail.com>
2020-01-07 09:32:49 -0400

    Added a logger for requests

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 var app = express()
 
+app.use((req,res,next)=>{
+  console.log("Requested " + req.path)
+  next()
+})
 app.use(express.static('public'))
 
 app.listen(8080)
