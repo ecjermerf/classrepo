f1eabdac07da6b51d54e5f4320d1b64bef20c1a7
Jermaine Francoeur <jermerf@trios-j5u0tel3s.trios.com>
2020-01-07 09:39:44 -0400

    Added admin route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 var app = express()
 
+app.get("/admin", (req, res) => {
+	res.send("Hello your majesty")
+})
+
 app.use(express.static('public'))
 
 app.listen(8080)
